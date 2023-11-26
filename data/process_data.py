e1b4b6edf65845cef63bde7b2dc3bb9d3c661813
nathi3712 <143811213+nathi3712@users.noreply.github.com>
2023-11-26 14:56:45 +0200

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -30,6 +30,8 @@ def clean_data(df):
     df.drop(["categories"], axis = 1, inplace = True)
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df, categories2], axis = 1, sort = False)
+    #Replacing 2 with 1 using the lambda function
+    df['related'] = df['related'].map(lambda x : 1 if (x == 2) else x)
     # drop duplicates
     df.drop_duplicates(inplace = True)
     
@@ -38,7 +40,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('MessagesTables', engine, index = False) 
+    df.to_sql('MessagesTables', engine, index = False, if_exists = 'replace') 
 
 
 def main():
